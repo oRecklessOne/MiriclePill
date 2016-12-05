47b3f695a2dc543879d16de39e4961ef7e644b57
oliver drabble <ollie@olivers-MacBook-Pro.local>
2016-12-05 18:01:43 +0000

    Initial Commit

---
+++ MiriclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiriclePill
+//
+//  Created by oliver drabble on 05/12/2016.
+//  Copyright © 2016 Reckless Games Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
